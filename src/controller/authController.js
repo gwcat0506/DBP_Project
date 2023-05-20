eb6fb3493401e0e48cdd83a4c9bd41c5cd8aa5b8
gwcat0506 <61350052+gwcat0506@users.noreply.github.com>
2023-05-21 01:22:26 +0900

    로그인 토큰 구현

--- src/controller/authController.js
+++ src/controller/authController.js
@@ -31,6 +31,9 @@ const signupUser = async (req, res) => {
 };
 
 // 로그인
+const jwt = require("jsonwebtoken");
+require('dotenv').config();
+
 const loginUser = async (req, res) => {
   const { user_id, password } = req.body;
   if (!user_id || !password)
@@ -38,9 +41,25 @@ const loginUser = async (req, res) => {
 
   const check = await authService.loginUser(user_id, password);
   if (check){
-    return res.status(200).json({ status: 200, success: true, message: "로그인 성공" });
+    const docs = req.body;
+    const payload = { // putting data into a payload
+        docs,
+    };
+    // generating json web token and sending it
+    jwt.sign(
+      payload, // payload into jwt.sign method
+      process.env.SECRET_KEY, // secret key value
+      { expiresIn: "30m" }, // token expiration time
+      (err, token) => {
+          if (err) throw err;
+          else {
+              return res.status(200).json({ status: 200, success: true, message: "로그인 성공",result:token })
+              .cookie('user', token,{maxAge:30*60 * 1000}) // 1000 is a sec
+              .end()
+              
+  }});
   }else{
-    return res.status(500).json({ status: 500, success: false, message: "로그인 실패" });}
+    return res.status(500).json({ status: 400, success: false, message: "로그인 실패" });}
   // try {
   //   if (check){
   //     // 로그인 성공시 토큰발급
