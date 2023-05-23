1f598457b29f1fade96de34e9d8cdf52bbbbc80d
gwcat0506 <61350052+gwcat0506@users.noreply.github.com>
2023-05-23 21:07:43 +0900

    로그인/회원가입 라우팅

--- app.js
+++ app.js
@@ -11,6 +11,25 @@ app.use("/", router);
 app.get("/", (req, res) => {
   res.sendFile(__dirname+"/src/public/login.html");
 });
+app.get("/signup", (req, res) => {
+  res.sendFile(__dirname+"/src/public/signup.html");
+});
+app.get("/main", (req, res) => {
+  res.sendFile(__dirname+"/src/public/main.html");
+});
+
+// import routers
+// const loginRouter = require('./public/login');
+// const articleRouter = require('./routes/article');
+// const uploadRouter = require('./routes/upload');
+
+// 요청 경로에 따라 router 실행
+// app.use('/', loginRouter);
+// app.use('/article', articleRouter);
+// app.use('/upload', uploadRouter);
+// app.get("/signup", (req, res) => {
+//   res.sendFile(__dirname+"/src/public/signup.html");
+// });
 
 app.listen(PORT, () => {
   console.log(`
