2c4e571bcd8f7ba65ceca46fde39f0d00304ac42
wkdyujin <yujin1203713@gmail.com>
2023-05-26 13:39:15 +0900

    사원 정보 수정

--- src/controller/employeeController.js
+++ src/controller/employeeController.js
@@ -16,4 +16,12 @@ const searchEmployee = async (req, res) => {
     .json({ status: 200, message: "사원 검색 성공", result });
 };
 
-module.exports = { searchEmployee };
+const updateEmployee = async (req, res) => {
+  const { e_id } = req.params;
+  const { salary, status } = req.body;
+  const result = await employeeService.updateEmployee(e_id, salary, status);
+
+  return res.status(200).json({ status: 200, message: "사원 정보 수정 성공" });
+};
+
+module.exports = { searchEmployee, updateEmployee };
--- src/router/employeeRouter.js
+++ src/router/employeeRouter.js
@@ -3,6 +3,7 @@ const { employeeController } = require("../controller/");
 
 const router = Router();
 
+router.patch("/:e_id", employeeController.updateEmployee);
 router.get("/", employeeController.searchEmployee);
 
 module.exports = router;
