119e6bf506b9ffa7579c38384a6ec9a1a3f7201f
wkdyujin <yujin1203713@gmail.com>
2023-05-28 14:49:36 +0900

    [Feat] 로그인 페이지 경고 메시지 추가

--- src/controller/authController.js
+++ src/controller/authController.js
@@ -1,52 +1,70 @@
 const { Request, Response, NextFunction } = require("express");
 const { authService } = require("../service");
 
-
 // 아이디 중복 확인
 const checkUser = async (req, res) => {
   const { user_id } = req.body;
-  if (!user_id )
-    return res.status(400).json({ status: 400, success: false, message: "아이디를 입력해주세요." });
+  if (!user_id)
+    return res
+      .status(400)
+      .json({ status: 400, success: false, message: "아이디를 입력해주세요." });
 
   const check = await authService.checkUser(user_id);
-  console.log(check)
+  console.log(check);
   if (check)
-    return res.status(400).json({ status: 400, success: false, message: "이미 중복되는 아이디가 존재합니다." });
+    return res.status(400).json({
+      status: 400,
+      success: false,
+      message: "이미 중복되는 아이디가 존재합니다.",
+    });
 
-  return res.status(200).json({ status: 200, success: true, message: "사용 가능한 아이디입니다." });
+  return res
+    .status(200)
+    .json({ status: 200, success: true, message: "사용 가능한 아이디입니다." });
 };
 
 // 회원가입(DB저장)
 const signupUser = async (req, res) => {
   const { user_num, user_id, password } = req.body;
   if (!user_num || !user_id || !password)
-    return res.status(400).json({ status: 404, message: "모든 값을 입력해주세요." });
+    return res
+      .status(400)
+      .json({ status: 404, message: "모든 값을 입력해주세요." });
 
   const check = await authService.signupUser(user_num, user_id, password);
   try {
-    if (check){
-      return res.status(200).json({ status: 200, success: true, message: "회원가입 성공" });}
+    if (check) {
+      return res
+        .status(200)
+        .json({ status: 200, success: true, message: "회원가입 성공" });
+    }
   } catch {
-      return res.status(500).json({ status: 500, success: false, message: "회원가입 실패" });}
+    return res
+      .status(500)
+      .json({ status: 500, success: false, message: "회원가입 실패" });
+  }
 };
 
 // 로그인
 const jwt = require("jsonwebtoken");
-require('dotenv').config();
+require("dotenv").config();
 
 const loginUser = async (req, res) => {
   const { user_id, password } = req.body;
-  console.log(user_id,password)
+  console.log(user_id, password);
 
   if (!user_id || !password)
-    return res.status(400).json({ status: 404, message: "모든 값을 입력해주세요." });
+    return res
+      .status(400)
+      .json({ status: 404, message: "아이디와 비밀번호를 입력해주세요." });
 
   const check = await authService.loginUser(user_id, password);
-  console.log(check)
-  if (check){
+  console.log(check);
+  if (check) {
     const docs = req.body;
-    const payload = { // putting data into a payload
-        docs,
+    const payload = {
+      // putting data into a payload
+      docs,
     };
     // generating json web token and sending it
     jwt.sign(
@@ -54,18 +72,30 @@ const loginUser = async (req, res) => {
       process.env.SECRET_KEY, // secret key value
       { expiresIn: "30m" }, // token expiration time
       (err, token) => {
-          if (err) throw err;
-          else {
-              return res.status(200).json({ status: 200, success: true, message: "로그인 성공",result:token })
-              .cookie('user', token,{maxAge:30*60 * 1000}) // 1000 is a sec
-              .end()
-              
-  }});
-  }else{
-    return res.status(500).json({ status: 400, success: false, message: "로그인 실패" });}
+        if (err) throw err;
+        else {
+          return res
+            .status(200)
+            .json({
+              status: 200,
+              success: true,
+              message: "로그인 성공",
+              result: token,
+            })
+            .cookie("user", token, { maxAge: 30 * 60 * 1000 }) // 1000 is a sec
+            .end();
+        }
+      }
+    );
+  } else {
+    return res
+      .status(400)
+      .json({
+        status: 400,
+        success: false,
+        message: "아이디와 비밀번호를 확인해주세요.",
+      });
+  }
 };
 
-
-module.exports = { checkUser,signupUser,loginUser };
-
-
+module.exports = { checkUser, signupUser, loginUser };
