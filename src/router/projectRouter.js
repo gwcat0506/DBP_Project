43f4f92e223bb456715003a1a66a99472ee92540
gwcat0506 <61350052+gwcat0506@users.noreply.github.com>
2023-05-28 18:52:27 +0900

    [Feat]프로젝트 인원 추가 구현

--- src/router/projectRouter.js
+++ src/router/projectRouter.js
@@ -5,7 +5,7 @@ const router = Router();
 
 router.get("/:projectId", projectController.getProjectById);
 router.get("/", projectController.searchProjects);
-router.post("/putEmpoyee", projectController.putEmployees);
+router.post("/putEmployee", projectController.putEmployees);
 router.post("/", projectController.createProject);
 router.get("/putEmployee/deadline", projectController.searchDeadline);
 router.get("/putEmployee/career", projectController.searchCareer);
