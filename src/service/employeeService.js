7811ddf4114df1516bb60358e5ab4049f75f44b4
gwcat0506 <61350052+gwcat0506@users.noreply.github.com>
2023-05-29 14:15:51 +0900

    [Fix]직원 더보기 구현

--- src/service/employeeService.js
+++ src/service/employeeService.js
@@ -74,7 +74,7 @@ const searchEmployee = async (e_id, e_name, skills, career) => {
       e_name: emp.e_name,
       career: emp.career,
       skills: skillStr,
-      salary: formatCurrency(emp.salary),
+      salary: emp.salary,
       birthday: birthday,
       status: emp.status,
     };
@@ -91,10 +91,6 @@ const updateEmployee = async (e_id, salary, status) => {
     data: { salary: salary, status: status },
   });
   return data;
-};
-
-const formatCurrency = (num) => {
-  return num.toString().replace(/(\d)(?=(\d{3})+(?!\d))/g, "$1,");
 };
 
 module.exports = {
